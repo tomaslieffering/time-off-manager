2ed84bf88c1cffe0d857aaedb3ddbc42fb366b93
tomaslieffering <tomaslieffering@gmail.com>
2025-04-09 08:58:34 +0100

    Update .env.example

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.env.REACT_APP_BACKEND,
+	baseURL: 'http://ec2-16-171-33-96.eu-north-1.compute.amazonaws.com',
 	withCredentials: true,
 	withXSRFToken: true
 })
