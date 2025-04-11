41b50110017061d72ac8a251281506bea8936762
tomaslieffering <tomaslieffering@gmail.com>
2025-04-11 12:17:41 +0100

    Update cors config

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://timeoffmanager.local'],
+    'allowed_origins' => [env('FRONTEND_URL')],
 
     'allowed_origins_patterns' => [],
 
